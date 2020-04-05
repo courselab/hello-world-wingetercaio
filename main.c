5c5346e7d00389dddb2b6d5071ccc6c571a021b2
wingetercaio <62968121+wingetercaio@users.noreply.github.com>
2020-04-05 18:38:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -17,9 +17,11 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include <stdio.h>
 
 int main()
-{   printf ("Hello world");
-    return 0;
+{ 
+   printf ("Hello world");
+   return 0;
 }
